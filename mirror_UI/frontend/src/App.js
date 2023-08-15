ea386f908494bc8ecca728126cf78bf9761df02e
siddharth200119 <siddramnani@gmail.com>
2023-08-16 00:38:08 +0530

    implemented full screen

--- mirror_UI/frontend/src/App.js
+++ mirror_UI/frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import the CSS file you created
 
 function App() {
+  const requestFullScreen = () => {
+    const rootElement = document.documentElement;
+
+    if (rootElement.requestFullscreen) {
+      rootElement.requestFullscreen();
+    } else if (rootElement.mozRequestFullScreen) {
+      rootElement.mozRequestFullScreen();
+    } else if (rootElement.webkitRequestFullscreen) {
+      rootElement.webkitRequestFullscreen();
+    } else if (rootElement.msRequestFullscreen) {
+      rootElement.msRequestFullscreen();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='fullscreen-container'>
+        <button className='fullScreenBtn' onClick={requestFullScreen}>Go Full Screen</button>
+      </div>
+      
+      {/* Your app content */}
     </div>
   );
 }
