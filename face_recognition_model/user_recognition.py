e15a4fe69ef86e6aa7d62b4fbe87a2669462b465
siddharth200119 <siddramnani@gmail.com>
2023-09-16 16:11:45 +0530

    minor bug fix to detect user among multiple people

--- face_recognition_model/user_recognition.py
+++ face_recognition_model/user_recognition.py
@@ -8,11 +8,12 @@ def user_recognition(file, image_cv2):
 
     rgb_img = cv2.cvtColor(image_cv2, cv2.COLOR_BGR2RGB)
     try:  
-        cpm_img_encoding = face_recognition.face_encodings(rgb_img)[0]
+        cpm_img_encodings = face_recognition.face_encodings(rgb_img)
         for person in people:
-            if face_recognition.compare_faces([person["face_encodings"][0]], cpm_img_encoding)[0]:
-                return(person['name'])
-            else:
-                continue
+            for encoding in cpm_img_encodings:
+                if face_recognition.compare_faces([person["face_encodings"][0]], encoding)[0]:
+                    return(person['name'])
+                else:
+                    continue
     except:
         return("no face detected")
